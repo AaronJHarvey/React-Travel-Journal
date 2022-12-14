1317cb13ae742b3fd8fec0cf56f549b01d987597
Aaron <Harvey.AaronJ@gmail.com>
2022-12-14 12:50:59 -0500

    Builds hardcoded entry component

--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
-export default function Entry () {
-	return <h1>Entry information here</h1>;
+export default function Entry (props) {
+	// console.log(props)
+	return (
+		<div className='entry'>
+			<img src={props.imageUrl} />
+			<div className='location-info'>
+				<h3>{props.location}</h3>
+				<a href={props.googleMapsUrl}>
+					<h3>View on Google Maps</h3>
+				</a>
+			</div>
+			<h1 className='Entry-Title'>{props.title}</h1>
+			<h4 className='dates'>
+				{props.startDate} - {props.endDate}
+			</h4>
+			<p className='description'>{props.description}</p>
+		</div>
+	);
 }
+
+// title
+// location
+// googleMapsUrl
+// startDate
+// endDate
+// description
+// imageUrl
