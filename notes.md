facdf36bb874257c5cff610e53da56c304c0d8ed
Aaron <Harvey.AaronJ@gmail.com>
2022-12-14 11:20:51 -0500

    Builds Data.js file to be used as props

---
+++ notes.md
@@ -0,0 +1,14 @@
+Building a Travel Journal
+
+Navbar/header -
+    - Logo of some sort 
+    - Title 
+
+Body
+    - Entries 
+        - Photo 
+        - Destination 
+        - Link to location on google maps? 
+        - Date of Travel 
+        - Description of Location or recap of trip 
+        
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		title: 'Pigeon Forge',
+		location: 'Tennessee',
+		googleMapsUrl: 'https://goo.gl/maps/wcvTsAnXoNTDhpmq5',
+		startDate: 'March 24, 2022',
+		endDate: 'March 29, 2022',
+		description:
+			'Pigeon Forge, a mountain town and vacation area in eastern Tennessee, is the home of Dollywood, country singer Dolly Parton"s Appalachian-themed park consisting of rides, an adjoining water park and a museum of her costumes and memorabilia. The city’s other attractions include country music revues like the Smoky Mountain Opry, dinner theaters such as Dolly Parton"s Dixie Stampede and outlet malls.',
+		imageUrl: 'dfee'
+	},
+	{
+		title: 'DC',
+		location: 'Washington, DC',
+		googleMapsUrl: 'https://goo.gl/maps/ZasVANDDBgkSmiiE7',
+		startDate: 'October 21, 2022',
+		endDate: 'October 24, 2022',
+		description:
+			"Washington, DC, the U.S. capital, is a compact city on the Potomac River, bordering the states of Maryland and Virginia. It's defined by imposing neoclassical monuments and buildings - including the iconic ones that house the federal government's 3 branches: the Capitol, White House and Supreme Court. It's also home to iconic museums and performing-arts venues such as the Kennedy Center.",
+		imageUrl: 'fef'
+	},
+	{
+		title: 'Mount Fuji',
+		location: 'Japan',
+		googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+		startDate: '12 Jan, 2021',
+		endDate: '24 Jan, 2021',
+		description:
+			'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+		imageUrl: 'src/Images/Mt-Fuji.jpeg'
+	}
+];
