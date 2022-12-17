7f762179284bc9135c961fbb92b111707dd59141
Aaron <Harvey.AaronJ@gmail.com>
2022-12-16 23:03:19 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,6 @@ function App () {
 				description='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
 				imageUrl='src/Images/Mt-Fuji.jpeg'
 			/>
-			<Entry
-				title='Mount Fuji'
-				location='Japan'
-				googleMapsUrl='https://goo.gl/maps/1DGM5WrWnATgkSNB8'
-				startDate='January 12, 2021'
-				endDate='January 24, 2021'
-				description='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
-				imageUrl='src/Images/Mt-Fuji.jpeg'
-			/>
-			<Entry
-				title='Mount Fuji'
-				location='Japan'
-				googleMapsUrl='https://goo.gl/maps/1DGM5WrWnATgkSNB8'
-				startDate='January 12, 2021'
-				endDate='January 24, 2021'
-				description='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
-				imageUrl='src/Images/Mt-Fuji.jpeg'
-			/>
 		</div>
 	);
 }
--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -4,8 +4,8 @@ export default function Entry (props) {
 	// console.log(props)
 	return (
 		<div className='entry'>
-			<img src={props.imageUrl} />
-			<div className='location-info'>
+			<img className='photo' src={props.imageUrl} />
+			{/* <div className='location-info'>
 				<h3>{props.location}</h3>
 				<a href={props.googleMapsUrl}>
 					<h3>View on Google Maps</h3>
@@ -15,7 +15,7 @@ export default function Entry (props) {
 			<h4 className='dates'>
 				{props.startDate} - {props.endDate}
 			</h4>
-			<p className='description'>{props.description}</p>
+			<p className='description'>{props.description}</p> */}
 		</div>
 	);
 }
