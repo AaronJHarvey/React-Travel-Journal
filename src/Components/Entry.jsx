25f02bbd9909cf1cace800048cf54f8ae315fdfa
Aaron <Harvey.AaronJ@gmail.com>
2022-12-18 00:38:32 -0500

    Styles .location-info

--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -5,17 +5,17 @@ export default function Entry (props) {
 	return (
 		<div className='entry'>
 			<img className='photo' src={props.imageUrl} />
-			{/* <div className='location-info'>
+			<div className='location-info'>
 				<h3>{props.location}</h3>
 				<a href={props.googleMapsUrl}>
 					<h3>View on Google Maps</h3>
 				</a>
 			</div>
-			<h1 className='Entry-Title'>{props.title}</h1>
-			<h4 className='dates'>
-				{props.startDate} - {props.endDate}
-			</h4>
-			<p className='description'>{props.description}</p> */}
+			{/* <h1 className='Entry-Title'>{props.title}</h1> */}
+			{/* <h4 className='dates'> */}
+			{/* {props.startDate} - {props.endDate} */}
+			{/* </h4> */}
+			{/* <p className='description'>{props.description}</p> */}
 		</div>
 	);
 }
