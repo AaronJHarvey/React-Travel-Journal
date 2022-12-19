3707201077ba4abd2f4382522a4ac57885df6821
Aaron <Harvey.AaronJ@gmail.com>
2022-12-18 23:14:28 -0500

    Styles title

--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -6,18 +6,21 @@ export default function Entry (props) {
 	return (
 		<div className='entry'>
 			<img className='photo' src={props.imageUrl} />
-			<div className='location-info'>
-				<CiLocationOn id='location-icon' />
-				<h3 id='location-name'>{props.location}</h3>
-				<a href={props.googleMapsUrl}>
-					<h3>View on Google Maps</h3>
-				</a>
+
+			<div className='text-section'>
+				<div className='location-info'>
+					<CiLocationOn id='location-icon' />
+					<h3 id='location-name'>{props.location}</h3>
+					<a href={props.googleMapsUrl}>
+						<h3>View on Google Maps</h3>
+					</a>
+				</div>
+				<h1 className='Entry-Title'>{props.title}</h1>
+				{/* <h4 className='dates'> */}
+				{/* {props.startDate} - {props.endDate} */}
+				{/* </h4> */}
+				{/* <p className='description'>{props.description}</p> */}
 			</div>
-			{/* <h1 className='Entry-Title'>{props.title}</h1> */}
-			{/* <h4 className='dates'> */}
-			{/* {props.startDate} - {props.endDate} */}
-			{/* </h4> */}
-			{/* <p className='description'>{props.description}</p> */}
 		</div>
 	);
 }
